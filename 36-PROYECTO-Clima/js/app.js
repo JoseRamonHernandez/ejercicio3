75dc59167451ce96fff35764a9ccb3de47d91fd5
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-09-15 21:59:20 -0500

    Video 224
    Validando formulario

--- 36-PROYECTO-Clima/js/app.js
+++ 36-PROYECTO-Clima/js/app.js
@@ -10,5 +10,39 @@ const formulario = document.querySelector('#formulario');
  function buscarClima(e){
      e.preventDefault();
 
-     console.log('Buscando Clima...');
+    //validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
+
+    if(ciudad === '' || pais === '')
+    {
+        mostrarError('Ambos campos son obligatorios');
+        return;
+    }
+    
  }
+
+ function mostrarError(mensaje){
+   const alerta = document.querySelector('.bg-red-100');
+
+   if(!alerta){
+     // crear alerta
+
+     const alerta = document.createElement('div');
+     alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounend', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+     alerta.innerHTML=`
+     <strong class="font-bold">Error!!</strong>
+     <span class="block">${mensaje}</span>
+     
+     `;
+
+     container.appendChild(alerta);
+
+     //eliminar la alerta despues de 5 segundos
+     setTimeout(() =>{
+         alerta.remove();
+     }, 5000);
+ }
+
+}
